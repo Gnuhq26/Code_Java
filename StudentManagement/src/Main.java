4fd7fbb73f054d60a8d9260529d60eda3a692f46
Gnuhq26 <gnuhq26@gmail.com>
2024-08-11 16:17:45 +0700

    upload files

--- StudentManagement/src/Main.java
+++ StudentManagement/src/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        saveAStudent();
-        inputStudent();
+        //saveAStudents();
+        //inputStudent();
     }
 
     //muốn nhập info từ bp, ta cần phương tiện nhập, OOP cái gì cx phải là object
@@ -47,9 +47,36 @@ public class Main {
         x.showProfile();
     }
     //hard-code fix luôn info sv, ko đã, ước gì nhập từ bàn phím, cũng phải đổ vào Khuôn/phễu
-    public static void saveAStudent() {
+    public static void saveAStudents() {
         Student an = new Student("20225849", "HungBQ", 2004, 3.14);
+        Student Bom = new Student("20225848", "Hungsieudz", 2009, 3.16);
         an.showProfile();
+        Bom.showProfile();
+
+        System.out.println("Check the public/private");
+        //xem chấm khi có public trong khuôn;
+        String s1Name = an.name;
+        System.out.println("an name: " + s1Name);
+        System.out.println(an.getName());
+        System.out.println(an.name);
+        //khi sờ trực tiếp đặc điểm, tức là ta chạm trực tiếp biến, chạm biến ~ get() set ()
+
+        an.name = "Siêu chim to";
+        an.setName("Chim to nữa");
+        an.showProfile();
+        Bom.showProfile();
+        //static là vùng nhớ vùng chung cho các object cùng loại
+        //ai cùng nghĩ là của mình, nhưng thực tế là chung
+        //nếu 1 ai đó tác động, thì tất cả bị ảnh hưởng
+        //giá trị cuar vùng static chung cho tất cả các object hiện tại và tương lai,
+        //WC tập thể chung cho cả dãy lầu
+        //sờ static đừng sờ qua kiểu tư hữu mà nên sờ theo kiểu Class chấm Static
+        System.out.println("Student name: " + Student.name);
+        //có 2 loại chấm: chấm qua biến object hàm ý non-static, đồ thuộc từng object
+        //chấm thứ 2: đồ static xài chung, new cả tỉ lần cũng chỉ có 1 vùng ram, chung cả đám
+        //vậy chấm qua cả đám hợp lí hơn
+
+
     }
 
 }
--- StudentManagement/src/Student.java
+++ StudentManagement/src/Student.java
@@ -1,8 +1,8 @@
 public class Student {
-    private String id;
-    private String name;
-    private int yob;
-    private double gpa;
+    private static String id;
+    public static String name;
+    private int yob; //non-static
+    public double gpa; //non-static
 
     public Student(String id, String name, int yob, double gpa) {
         this.id = id;
@@ -43,4 +43,13 @@ public class Student {
     public void showProfile() {
         System.out.printf("|%8s|%-25s|%4d|%4.1f|\n", id, name, yob, gpa);
     }
+    //non-static chơi với non-static là rõ ràng vì cùng khu vực,
+    //và chơi cả với static vì đó là 1 phần tách ra
+
+    //chơi hàm static xem sao
+    public static void sayHi() {
+        System.out.println("Hi my name is " + name);
+        //System.out.println("By the way, my yob is: " + yob);
+        //ko biết yob nào để mà in vì clone nhiều yob quá, vì nhiều sv
+    }   //static chỉ chơi static vì cùng khu vực
 }
